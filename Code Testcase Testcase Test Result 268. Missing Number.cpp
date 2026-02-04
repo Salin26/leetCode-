c23cb79315569dbcfb34d23bb645765dd584a826
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2026-02-04 16:32:59 +0600

    Create Code Testcase Testcase Test Result 268. Missing Number.cpp

---
+++ Code Testcase Testcase Test Result 268. Missing Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int n=nums.size();
+    map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+       mp[nums[i]]++;
+    }
+    for(int i=0;i<1000000;i++)
+    {
+     if(mp[i]<=0)
+     {
+      return i;
+    }
+  }
+        return -1;
+    }
+};
